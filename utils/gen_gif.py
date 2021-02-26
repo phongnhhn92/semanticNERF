7bfc4f993ab4af61b374596105d199d86cc67cb0
phong <phongnhhn92@gmail.com>
2021-02-26 17:25:34 +0200

    Dual path semanticNERF

--- utils/gen_gif.py
+++ utils/gen_gif.py
@@ -7,8 +7,9 @@ PATH = '/media/phong/Data2TB/Paper4_backup/26-02-2020_NERF_semantic_consitent/NE
 
 imgs = []
 for n,file in enumerate(glob.glob(os.path.join(PATH,"*.png"))):
+    print(file)
     img =  np.asarray(Image.open(file))
     imgs += [img]
 
-imageio.mimsave('out.gif',imgs,fps=30)
+imageio.mimsave('out.gif',imgs,fps=10)
 
