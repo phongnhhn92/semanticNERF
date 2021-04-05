262f4f55d0bc9dbf3df8cf44baaa7c50b4bccf43
phong <phongnhhn92@gmail.com>
2021-04-05 19:52:44 +0300

    update

--- train_puhti.sh
+++ train_puhti.sh
@@ -13,4 +13,4 @@ module load gcc/8.3.0 cuda/10.1.168 cudnn cmake
 
 python main.py --dataset_name blender --root_dir /media/phong/Data2TB/dataset/NERF/nerf_synthetic/nerf_synthetic/lego \
 --N_importance 128 --img_wh 800 800 --noise_std 0 --num_epochs 16 --batch_size 4096 --optimizer adam --lr 5e-4 \
---lr_scheduler steplr --decay_step 2 4 --decay_gamma 0.5 --exp_name exp_lego
+--lr_scheduler steplr --decay_step 2 4 8 --decay_gamma 0.5 --exp_name exp_lego
