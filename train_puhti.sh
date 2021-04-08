44cc377e878f32ca862647e4c1c985582f561cbf
phong <phongnhhn92@gmail.com>
2021-04-08 11:14:37 +0300

    update code

--- train_puhti.sh
+++ train_puhti.sh
@@ -6,12 +6,12 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
 #SBATCH --partition=gpu
-#SBATCH --gres=gpu:v100:1
-#SBATCH -o train_o.txt
-#SBATCH -e train_e.txt
+#SBATCH --gres=gpu:v100:4
+#SBATCH -o train_o_carla.txt
+#SBATCH -e train_e_carla.txt
 module load gcc/8.3.0 cuda/10.1.168 cudnn cmake
 
 
-python main.py --dataset_name carla --root_dir /media/phong/Data2TB/dataset/carla/carla/carla_phong_2/Town01/episode_00001/000000 \
-               --N_importance 64 --img_wh 800 600 --noise_std 0 --num_epochs 5 --batch_size 1024 --optimizer adam --lr 5e-4 \
-               --lr_scheduler steplr --decay_step 2 4 --decay_gamma 0.5 --exp_name exp_carla
+python main.py --dataset_name carla --num_gpus 4 --root_dir /scratch/project_2001055/dataset/carla/000000 \
+               --N_importance 64 --img_wh 800 600 --noise_std 0 --num_epochs 10 --batch_size 4096 --optimizer adam --lr 5e-4 \
+               --lr_scheduler steplr --decay_step 2 4 8 --decay_gamma 0.5 --exp_name exp_carla
