ae1628531edbdc8f92ea62cef0e83785f9a3da37
phong_ASUS <phongnhhn92@gmail.com>
2021-04-20 22:43:46 +0300

    update train_puhti.sh

--- train_puhti.sh
+++ train_puhti.sh
@@ -13,7 +13,6 @@ module load gcc/8.3.0 cuda/10.1.168 cudnn cmake
 
 srun python main.py --dataset_name carlaGVS --root_dir /scratch/project_2001055/dataset/GVS \
                --log_dir /scratch/project_2001055/trained_logs/semanticNERF/logs \
-               --use_vae False --use_disparity_loss False \
                --N_importance 96 --img_wh 256 256 --noise_std 0.1 --num_epochs 30 --batch_size 8 \
                --num_rays 1024 --optimizer adam --lr 4e-5 --lr_scheduler steplr --decay_step 10 15 20 \
                --decay_gamma 0.5 --exp_name exp_carla_GVS
