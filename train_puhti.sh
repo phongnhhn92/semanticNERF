4c5b184f9794b8edf1132b6a9136d869d3ec1616
phong <phongnhhn92@gmail.com>
2021-04-22 16:06:44 +0300

    update code

--- train_puhti.sh
+++ train_puhti.sh
@@ -15,4 +15,4 @@ srun python main.py --dataset_name carlaGVS --root_dir /scratch/project_2001055/
                --log_dir /scratch/project_2001055/trained_logs/semanticNERF/logs \
                --N_importance 96 --img_wh 256 256 --noise_std 0.1 --num_epochs 20 --batch_size 8 \
                --num_rays 1024 --optimizer adam --lr 4e-5 --lr_scheduler steplr --decay_step 7 15 \
-               --decay_gamma 0.5 --use_disparity_loss --exp_name exp_carla_GVS_wResNet
+               --decay_gamma 0.5 --exp_name exp_carla_GVS_wResNet
