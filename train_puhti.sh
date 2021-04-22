249ff19c06d8eb00b9fc215ffe7c25a8c6d99c75
phong <phongnhhn92@gmail.com>
2021-04-22 11:03:53 +0300

    fix DEBUG

--- train_puhti.sh
+++ train_puhti.sh
@@ -13,6 +13,6 @@ module load gcc/8.3.0 cuda/10.1.168 cudnn cmake
 
 srun python main.py --dataset_name carlaGVS --root_dir /scratch/project_2001055/dataset/GVS \
                --log_dir /scratch/project_2001055/trained_logs/semanticNERF/logs \
-               --N_importance 96 --img_wh 256 256 --noise_std 0.1 --num_epochs 30 --batch_size 8 \
-               --num_rays 1024 --optimizer adam --lr 4e-5 --lr_scheduler steplr --decay_step 10 15 20 \
+               --N_importance 96 --img_wh 256 256 --noise_std 0.1 --num_epochs 20 --batch_size 8 \
+               --num_rays 1024 --optimizer adam --lr 4e-5 --lr_scheduler steplr --decay_step 7 15 \
                --decay_gamma 0.5 --use_disparity_loss --exp_name exp_carla_GVS_wResNet
