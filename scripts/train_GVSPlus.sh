492660f188f3eec71ccfdf2d22ea57647bc90d51
phong <phongnhhn92@gmail.com>
2021-05-06 09:59:35 +0300

    update sh

--- scripts/train_GVSPlus.sh
+++ scripts/train_GVSPlus.sh
@@ -12,7 +12,7 @@
 module load gcc/8.3.0 cuda/10.1.168 cudnn cmake
 
 srun python ../train_GVSNETPlus.py --num_gpus 4 --dataset_name carlaGVS --root_dir /scratch/project_2001055/dataset/GVS \
-  --log_dir /scratch/project_2001055/trained_logs/semanticNERF/logs \
+  --log_dir /scratch/project_2001055/trained_logs/GVSPlus_NERF_MPIfeature \
   --img_wh 256 256 --noise_std 0.1 --num_epochs 30 --batch_size 4 --num_rays 4096 --N_importance 96 \
   --optimizer adam --lr 5e-4 --lr_scheduler steplr --decay_step 10 25 --decay_gamma 0.5 \
   --use_disparity_loss --exp_name exp_carla_GVSPlus
@@ -21,5 +21,5 @@ srun python ../train_GVSNETPlus.py --num_gpus 4 --dataset_name carlaGVS --root_d
 #python train_GVSNETPlus.py --num_gpus 2 --dataset_name carlaGVS --root_dir /mnt/disk1/dataset/GVS \
 #  --log_dir /mnt/disk1/dataset/GVS/logs_GVSNetPlus_MPI \
 #  --img_wh 256 256 --noise_std 0.1 --num_epochs 30 --batch_size 2 --num_rays 4096 --N_importance 96 \
-#  --optimizer adam --lr 5e-4 --lr_scheduler steplr --decay_step 10 25 --decay_gamma 0.5 \
+#  --optimizer adam --lr 2e-4 --lr_scheduler steplr --decay_step 10 25 --decay_gamma 0.5 \
 #  --use_disparity_loss --exp_name exp_carla_GVSPlus
