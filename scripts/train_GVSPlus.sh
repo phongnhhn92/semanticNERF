e31984a9798c0f8ad0d54223ad8bfccb9f50e9bd
phong <phongnhhn92@gmail.com>
2021-05-06 08:59:17 +0300

    update sh

--- scripts/train_GVSPlus.sh
+++ scripts/train_GVSPlus.sh
@@ -16,3 +16,10 @@ srun python ../train_GVSNETPlus.py --num_gpus 4 --dataset_name carlaGVS --root_d
   --img_wh 256 256 --noise_std 0.1 --num_epochs 30 --batch_size 4 --num_rays 4096 --N_importance 96 \
   --optimizer adam --lr 5e-4 --lr_scheduler steplr --decay_step 10 25 --decay_gamma 0.5 \
   --use_disparity_loss --exp_name exp_carla_GVSPlus
+
+
+#python train_GVSNETPlus.py --num_gpus 2 --dataset_name carlaGVS --root_dir /mnt/disk1/dataset/GVS \
+#  --log_dir /mnt/disk1/dataset/GVS/logs_GVSNetPlus_MPI \
+#  --img_wh 256 256 --noise_std 0.1 --num_epochs 30 --batch_size 2 --num_rays 4096 --N_importance 96 \
+#  --optimizer adam --lr 5e-4 --lr_scheduler steplr --decay_step 10 25 --decay_gamma 0.5 \
+#  --use_disparity_loss --exp_name exp_carla_GVSPlus
