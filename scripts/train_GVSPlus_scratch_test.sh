05e18440cdcf39c71e63ed57498d0f97c9d8d4c3
phong_ASUS <phongnhhn92@gmail.com>
2021-05-17 11:08:11 +0300

    update alpha clone and detach.

--- scripts/train_GVSPlus_scratch_test.sh
+++ scripts/train_GVSPlus_scratch_test.sh
@@ -14,6 +14,6 @@ module load gcc/8.3.0 cuda/10.1.168 cudnn cmake
 srun python ../train_GVSNETPlus.py --num_gpus 4 --dataset_name carlaGVS --root_dir /scratch/project_2001055/dataset/GVS \
   --log_dir /scratch/project_2001055/trained_logs/GVSPlus \
   --img_wh 256 256 --noise_std 0.1 --num_epochs 45 --batch_size 4 --num_rays 4096 --N_importance 32 \
-  --optimizer adam --lr 4e-4 --lr_scheduler steplr --decay_step 10 20 --decay_gamma 0.5 \
+  --optimizer adam --lr 5e-4 --lr_scheduler steplr --decay_step 10 20 --decay_gamma 0.5 \
   --exp_name exp_GVSPlus_SemanticRGBLoss_scratchSUN
 
